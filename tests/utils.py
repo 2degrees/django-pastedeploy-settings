6b061589921bdebafd5f269f257c0229f8bd0f0d
Gustavo Narea <gustavonarea@2degreesnetwork.com>
2013-03-13 16:37:42 +0000

    Removed test utilities used by twod.wsgi only

--- tests/utils.py
+++ tests/utils.py
@@ -4,8 +4,9 @@
 # Copyright (c) 2010, 2013, 2degrees Limited.
 # All Rights Reserved.
 #
-# This file is part of twod.wsgi <https://github.com/2degrees/twod.wsgi/>,
-# which is subject to the provisions of the BSD at
+# This file is part of django-pastedeploy-settings
+# <https://github.com/2degrees/django-pastedeploy-settings>, which is subject
+# to the provisions of the BSD at
 # <http://dev.2degreesnetwork.com/p/2degrees-license.html>. A copy of the
 # license should accompany this distribution. THIS SOFTWARE IS PROVIDED "AS IS"
 # AND ANY AND ALL EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT
@@ -13,21 +14,14 @@
 # INFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""
-Test suite for :mod:`twod.wsgi`.
-
-"""
-from StringIO import StringIO
 import logging
 import os
 
 import django.conf
 
 
-__all__ = ("BaseDjangoTestCase", "ClosingAppIter", "LoggingHandlerFixture",
-    "MOCK_WSGI_APP", "MockApp", "MockClosingApp", "MockGeneratorApp",
-    "MockLoggingHandler", "MockStartResponse", "MockWriteApp",
-    "complete_environ", )
+__all__ = ("BaseDjangoTestCase", "LoggingHandlerFixture", "MOCK_WSGI_APP",
+    "MockApp", "MockLoggingHandler")
 
 
 class BaseDjangoTestCase(object):
@@ -107,93 +101,5 @@ class MockApp(object):
 
 MOCK_WSGI_APP = MockApp('200 OK', ())
 
-
-class MockGeneratorApp(MockApp):
-    """
-    Mock WSGI application that returns an iterator.
-    
-    """
-
-    def __call__(self, environ, start_response):
-        self.environ = environ
-        start_response(self.status, self.headers)
-        def gen():
-            yield "body"
-            yield " as"
-            yield " iterable"
-        return gen()
-
-
-class MockWriteApp(MockApp):
-    """
-    Mock WSGI app which uses the write() function.
-    
-    """
-    
-    def __call__(self, environ, start_response):
-        self.environ = environ
-        write = start_response(self.status, self.headers)
-        write( "body")
-        write(" as")
-        write(" iterable")
-        return []
-
-
-class MockClosingApp(MockApp):
-    """Mock WSGI app whose response contains a close() method."""
-    
-    def __init__(self, *args, **kwargs):
-        super(MockClosingApp, self).__init__(*args, **kwargs)
-        self.app_iter = ClosingAppIter()
-    
-    def __call__(self, environ, start_response):
-        body = super(MockClosingApp, self).__call__(environ,start_response)
-        self.app_iter.extend(body)
-        return self.app_iter
-
-
-class ClosingAppIter(list):
-    """Mock response iterable with a close() method."""
-    
-    def __init__(self, *args, **kwargs):
-        super(ClosingAppIter, self).__init__(*args, **kwargs)
-        self.closed = False
-    
-    def close(self):
-        self.closed = True
-
-
-class MockStartResponse(object):
-    """Mock start_response() callable which keeps the arguments it receives."""
-    
-    def __init__(self):
-        self.status = None
-        self.response_headers = None
-        self.exc_info = None
-        self.called = False
-    
-    def __call__(self, status, response_headers, exc_info=None):
-        self.status = status
-        self.response_headers = response_headers
-        self.exc_info = exc_info
-        self.called = True
-
-
-def complete_environ(**environ):
-    """
-    Add the missing items in ``environ``.
-    
-    """
-    full_environ = {
-        'REQUEST_METHOD': "GET",
-        'SERVER_NAME': "example.org",
-        'SERVER_PORT': "80",
-        'SERVER_PROTOCOL': "HTTP/1.1",
-        'HOST': "example.org",
-        'wsgi.input': StringIO(""),
-        'wsgi.url_scheme': "http",
-        }
-    full_environ.update(environ)
-    return full_environ
 
 #}
